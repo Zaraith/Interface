5514a0b3011adff61516f1c4844929e7a913e8f2
Zaraith <anzoughet@gmail.com>
2018-06-11 14:25:23 +0200

    first commit

--- InterfaceGraph6.py
+++ InterfaceGraph6.py
@@ -52,12 +52,12 @@ app.config(menu=menubar)
 
 #Fonctions
 def Calculer(event):
-	Label5.configure(text='= '+str(eval(Entree.get())))
+	Label9.configure(text='= '+str(eval(Entree.get())))
 	
 
 #Table Statistique
 app.l = LabelFrame(app, text="Statistics", padx=15, pady=15)
-app.l.place(x=600, y=270, width=280, height=230)
+app.l.place(x=600, y=200, width=350, height=400)
 
 Activate = Checkbutton(app.l, text="Activate")
 Activate.grid(column=0, row=0)
@@ -71,21 +71,33 @@ Label2.grid(column=0, row=2)
 Label3 = Label(app.l, text="Z =", padx=5,pady=5)
 Label3.grid(column=0, row=3)
 
-Label4 = Label(app.l, text="Angle =", padx=15,pady=15)
+Label4 = Label(app.l, text="Min =", padx=15,pady=15)
 Label4.grid(column=0, row=4)
 
-Label5 = Label(app.l, padx=15,pady=15)
-Label5.grid(column=2, row=4)
+Label5 = Label(app.l, text="Max =", padx=15,pady=15)
+Label5.grid(column=0, row=5)
+
+Label6 = Label(app.l, text="Moyenne =", padx=15,pady=15)
+Label6.grid(column=0, row=6)
+
+Label7 = Label(app.l, text="SD =", padx=15,pady=15)
+Label7.grid(column=0, row=7)
+
+Label8 = Label(app.l, text="Angle =", padx=15,pady=15)
+Label8.grid(column=0, row=8)
+
+Label9 = Label(app.l, padx=15,pady=15)
+Label9.grid(column=4, row=8)
 
 Entree = Entry(app.l, background='white')
 Entree.bind("<Return>", Calculer)
-Entree.grid(column=1, row=4)
+Entree.grid(column=3, row=8)
 
 #----------------------------------------------------------------------------
 
 # Je mets le lien du fichier de données afin de créer 
 # ma courbe. Graphique 2D
-M = np.loadtxt('plot_data_file.txt')
+M = np.loadtxt('C:/Users/anzou/Documents/CoursM1/Python/Git/AlexandreDataWalkGraph/Sujet 01/Marker/staticnoise 29052018_001_2018_5_29 Marker30.txt')
 plt.plot(M[:,0], M[:,1], 'x', label='Movement')
 
 
@@ -93,13 +105,13 @@ title('Human Walk Analysis')
 
 
 # Limites de l'axe des abscisses
-plt.xlim(0, 10) 
+plt.xlim(-4950, -4960) 
 # Label de l'axe des abscisses                                  
 plt.xlabel('Temps (s)')           
 
 
 # Limites de l'axe des ordonnées
-plt.ylim(-10, 10)
+plt.ylim(953.5, 956)
 # Label de l'axe des ordonnées                                    
 plt.ylabel('Position (ms)')            
 
@@ -108,13 +120,13 @@ plt.ylabel('Position (ms)')
 # Préparer la figure 3D
 fig = plt.figure()
 ax = fig.gca(projection='3d')
-M = loadtxt('plot_data_file.txt')
+M = loadtxt('C:/Users/anzou/Documents/CoursM1/Python/Git/AlexandreDataWalkGraph/Sujet 01/Marker/staticnoise 29052018_001_2018_5_29 Marker30.txt')
 plt.plot(M[:,0], M[:,1], 'x')
 
 # Faire les limites de chaques axes
-ax.set_xlim((-100, 100))
-ax.set_ylim((-100, 100))
-ax.set_zlim((-100, 100))
+ax.set_xlim((-4950, -4960))
+ax.set_ylim((950, 960))
+ax.set_zlim((-1, 1))
 
  
 # Label de l'axe x                                 
