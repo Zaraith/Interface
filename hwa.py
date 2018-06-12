c1f639d9c2c75365c5a35c19928f0fd212a080ba
Zaraith <anzoughet@gmail.com>
2018-06-13 01:45:52 +0200

    hwa.py

--- hwa.py
+++ hwa.py
@@ -1,5 +1,6 @@
 from tkinter import Tk, Menu
-from tkinter.messagebox import askyesno
+from tkinter.messagebox import *
+from tkinter.filedialog import *
 
 
 class MainWindow(Tk):
@@ -8,13 +9,17 @@ class MainWindow(Tk):
         Tk.__init__(self)
         self.menubar = Menu(self)
         self.filemenu = Menu(self.menubar, tearoff=0)
-        self.filemenu.add_command(label="Quit", command=self.__do_filequit)
-        self.menubar.add_cascade(label="File", menu=self.filemenu)
+        self.filemenu.add_command(label='Open', command=self.__open_file)
+        self.filemenu.add_command(label='Quit', command=self.__do_filequit)
+        self.menubar.add_cascade(label='File', menu=self.filemenu)
         self.config(menu=self.menubar)
 
     def __do_filequit(self):
         if askyesno('Quit', 'Are you sure to quit ?'):
+            showwarning('Warning', 'Bye !')
             self.quit()
+    def __open_file(self):
+        filename = askopenfile(title='Choose a file', filetypes=[('TEXT files', '.txt')])
 
 
 if __name__ == '__main__':
