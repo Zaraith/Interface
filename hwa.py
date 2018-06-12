e9590bd358a6b82fb36e2871dc56f794393ef8ff
Christophe Magnani <christophe.magnani@gmail.com>
2018-06-12 22:40:08 +0200

    Implement File / Quit

--- hwa.py
+++ hwa.py
@@ -1,15 +1,20 @@
-import tkinter
+from tkinter import Tk, Menu
+from tkinter.messagebox import askyesno
 
 
-class MainWindow(tkinter.Tk):
+class MainWindow(Tk):
 
     def __init__(self):
-        tkinter.Tk.__init__(self)
-        self.menubar = tkinter.Menu(self)
-        self.menu = tkinter.Menu(self.menubar, tearoff=0)
-        self.menu.add_command(label="Quit")
-        self.menubar.add_cascade(label="File", menu=self.menu)
+        Tk.__init__(self)
+        self.menubar = Menu(self)
+        self.filemenu = Menu(self.menubar, tearoff=0)
+        self.filemenu.add_command(label="Quit", command=self.__do_filequit)
+        self.menubar.add_cascade(label="File", menu=self.filemenu)
         self.config(menu=self.menubar)
+
+    def __do_filequit(self):
+        if askyesno('Quit', 'Are you sure to quit ?'):
+            self.quit()
 
 
 if __name__ == '__main__':
