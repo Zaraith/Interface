d044fc8d30748d954e40de219689a0bf9be4b61a
Zaraith <anzoughet@gmail.com>
2018-06-14 15:08:35 +0200

    experiment.py

--- experiment.py
+++ experiment.py
@@ -1,6 +1,6 @@
 class Experiment:
 
     def load_data(self, filename):
-        file = open('test.txt', 'r')
+        file = open('C:/Users/anzou/Documents/CoursM1/Python/Git/AlexandreDataWalkGraph/Sujet 01/Marker/staticnoise 29052018_001_2018_5_29 Marker30.txt', 'r')
         print (file.read())
 
