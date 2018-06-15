7d3b242207566df6dbe3fd6c45f9efa400757f67
Zaraith <anzoughet@gmail.com>
2018-06-15 11:53:40 +0200

    experiment.py

--- experiment.py
+++ experiment.py
@@ -1,5 +1,15 @@
+import csv
+
+
 class Experiment:
 
     def load_data(self, filename):
         with open(filename, 'r') as file:
             print(file.read())
+
+    def file_from_txt_to_table():
+    	with open('C:/Users/anzou/Documents/CoursM1/Python/Git/staticnoise 29052018_001_2018_5_29 Marker30.txt', newline='') as csvfile:
+    		spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    		for row in spamreader:
+    			print(', '.join(row))
+
