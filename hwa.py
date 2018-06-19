c32e43816ead922e6fd9fe9fc783d38b740049a9
Zaraith <anzoughet@gmail.com>
2018-06-19 15:09:16 +0200

    hwa.py

--- hwa.py
+++ hwa.py
@@ -20,6 +20,7 @@ class MainWindow(Tk):
     def __create_menu(self):
         menubar = Menu(self)
         filemenu = Menu(menubar, tearoff=0)
+        filemenu.add_command(label='Open', command=self.__do_fileopen)
         filemenu.add_command(label='Quit', command=self.__do_filequit)
         menubar.add_cascade(label='File', menu=filemenu)
         self.config(menu=menubar)
@@ -27,7 +28,7 @@ class MainWindow(Tk):
     def __create_widget(self):
         label_frame = LabelFrame(self, text="X Curve", padx=30, pady=30)
         label_frame.place(x=15, y=15)
-        plot_time_button = Button(label_frame, text="Plot Time Domain", command=self.__do_fileopen)
+        plot_time_button = Button(label_frame, text="Plot Time Domain", command=self.__opengraphic)
         plot_time_button.grid(column=0, row=1)
         plot_frequency_button = Button(label_frame, text="Plot Frequency Domain")
         plot_frequency_button.grid(column=0, row=0)
@@ -40,6 +41,8 @@ class MainWindow(Tk):
         filename = askopenfilename(title='Select File',
                                    filetypes=[('Text files', '.txt')])
         self.__experiment.load_data(filename)
+
+    def __opengraphic(self):
         plt.plot(self.__experiment.x()[2:-1])
         plt.show()
 
